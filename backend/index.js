d565812b47e58a6ba6840527ed42117c172735e7
Kosta Arsic <battlefieldheroes177@gmail.com>
2023-03-26 23:58:42 +0200

    index.js

--- backend/index.js
+++ backend/index.js
@@ -11,18 +11,15 @@ dotenv.config({ path: "./config/.env" });
 connectDB();
 
 app.use(
-    cors({
-        origin: "*",
-    })
+  cors({
+    origin: "*",
+  })
 );
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // Middleware
@@ -37,13 +34,13 @@ app.use("/api/user", require("./routes/users"));
 
 // Errors
 app.use((err, req, res, next) => {
-    if (err.name === "UnauthorizedError") {
-        res.status(401).send({ valid: false });
-    } else {
-        next(err);
-    }
+  if (err.name === "UnauthorizedError") {
+    res.status(401).send({ valid: false });
+  } else {
+    next(err);
+  }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Running on port ${PORT}`));
