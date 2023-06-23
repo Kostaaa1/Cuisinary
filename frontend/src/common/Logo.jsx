faf016455f5d0e54bab1a16215dc7d92741e77f9
Kostaaa1 <kostaarsic123@gmail.com>
2023-06-23 23:55:55 +0200

    logo

--- frontend/src/common/Logo.jsx
+++ frontend/src/common/Logo.jsx
@@ -1,22 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { MdRiceBowl } from "react-icons/md";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { MdRiceBowl } from 'react-icons/md';
 
-const Logo = ({ style }) => {
+const Logo = ({ showSideNav, closeSideNav, style }) => {
+  const navigate = useNavigate();
+
   return (
-    <CulinaryLogo style={style} to={"/"}>
+    <CulinaryLogo
+      onClick={() => {
+        if (showSideNav) closeSideNav();
+        navigate('/');
+      }}
+      style={style}
+    >
       Cuisinary
       <MdRiceBowl className="logo" />
     </CulinaryLogo>
   );
 };
 
-const CulinaryLogo = styled(Link)`
+const CulinaryLogo = styled.div`
+  cursor: pointer;
   text-decoration: none;
   font-size: 2.2rem;
   font-weight: 900;
-  font-family: "Lobster Two", "Helvetica", sans-serif;
+  font-family: 'Lobster Two', 'Helvetica', sans-serif;
   font-style: italic;
   color: var(--red-color);
   display: flex;
