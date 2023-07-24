92414e86debe6ea1add7c8b204afa571a704b9bc
Kostaaa1 <kostaarsic123@gmail.com>
2023-07-24 23:21:32 +0200

    update

--- frontend/src/pages/profile/components/change-password/ChangePassword.jsx
+++ frontend/src/pages/profile/components/change-password/ChangePassword.jsx
@@ -1,25 +1,25 @@
-import { HttpsOutlined } from "@mui/icons-material";
-import Button from "../../../../common/Button";
-import { useNavigate } from "react-router-dom";
-import SectionHeader from "../../../../common/SectionHeader";
+import { HttpsOutlined } from '@mui/icons-material';
+import Button from '../../../../common/Button';
+import { useNavigate } from 'react-router-dom';
+import SectionHeader from '../../../../common/SectionHeader';
 
 const ChangeProfile = () => {
   const navigate = useNavigate();
   return (
-    <>
+    <div>
       <SectionHeader
         title="Change Password"
         text="The information on this page will be displayed on your
         public profile, which is visible to other users."
-        span="Your password will always remain private."
         icon={<HttpsOutlined />}
+        span="Your password will always remain private."
       />
       <Button
-        onClick={() => navigate("/account/forgot-password-reset")}
-        style={{ padding: "1.4rem 1.8rem" }}
-        value={"RESET PASSWORD"}
+        onClick={() => navigate('/account/forgot-password-reset')}
+        style={{ padding: '1.4rem 1.8rem' }}
+        value={'RESET PASSWORD'}
       />
-    </>
+    </div>
   );
 };
 
