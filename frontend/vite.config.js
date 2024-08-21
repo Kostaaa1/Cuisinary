1e112f57c6e9b7fb4f828d5ea57a435162641537
Kostaaa1 <kostaarsic123@gmail.com>
2024-08-21 08:55:07 +0200

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${mode === 'production' ? env.VITE_DEV_SERVER_URL : env.VITE_DEV_SERVER_URL}`;
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
